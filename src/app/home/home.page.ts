491a7e13c8725d7d8a69dc7d620d725a3966e7c4
Adrián Brito Pacheco <adrbrpa1988@gmail.com>
2018-10-09 13:17:54 +0200

    Updated employees list style

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  currentYear = new Date().getFullYear();
+  employees = [
+    {'employee_name': 'Adrián Brito Pacheco', 'job_position': 'Project Manager', 'avatar': 'http://i.pravatar.cc/150?img=1',
+     'description': 'Description'},
+    {'employee_name': 'José Antonio Pérez Florencia', 'job_position': 'Software Developer', 'avatar': 'http://i.pravatar.cc/150?img=2',
+    'description': 'Description'}
+  ];
+  
 }
