561baf8614a662baaff1fba2e2ba99ef5416ba5b
Adrián Brito Pacheco <adrbrpa1988@gmail.com>
2018-10-15 12:47:18 +0200

    Updated styles in employee details view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'detail', loadChildren: './detail/detail.module#DetailPageModule' },
+  { path: 'detail/:id', loadChildren: './detail/detail.module#DetailPageModule' },
 ];
 
 @NgModule({
--- src/app/detail/detail.page.ts
+++ src/app/detail/detail.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { StitchMongoServiceService } from '../services/stitch-mongo-service.service';
+
+import { AnonymousCredential} from 'mongodb-stitch-browser-sdk';
 
 @Component({
   selector: 'app-detail',
@@ -7,9 +12,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailPage implements OnInit {
 
-  constructor() { }
+  employee: any = null;
+
+  constructor(private route: ActivatedRoute, private stichMongoService: StitchMongoServiceService) { }
 
   ngOnInit() {
   }
+
+  ionViewWillEnter() {
+    // console.log('ionViewWillEnter');
+    const empployeeId = this.route.snapshot.paramMap.get('id');
+    // console.log('employeeId', empployeeId);
+    this.findEmployee(empployeeId);
+  }
+
+  findEmployee(employeId) {
+    this.stichMongoService.client.auth.loginWithCredential(new AnonymousCredential()).then(user => {
+      console.log('user', user);
+      return this.stichMongoService.find('employees', {'employee_name': employeId});
+      }
+    ).then(docs => {
+        // Collection is empty.
+        if (docs.length !== 0) {
+          // console.log(docs);
+          this.employee = docs[0];
+        }
+    }).catch(err => {
+        console.error(err);
+    });
+  }
+
 
 }
