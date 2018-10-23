5faed5770f766c0e96882a7e1fa3ab5a7e5d454a
Adrián Brito Pacheco <adrbrpa1988@gmail.com>
2018-10-23 12:07:03 +0200

    Updated organization chart component

--- src/app/components/organization-chart/organization-chart.component.ts
+++ src/app/components/organization-chart/organization-chart.component.ts
@@ -7,6 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrganizationChartComponent implements OnInit {
 
+  organization: any = {
+    'employee_name': '',
+    'employee_position': 'Director',
+    'employee_avatar': 'http://i.pravatar.cc/150?img=7',
+    'children': [
+      { 'employee_name': '', 'employee_position': 'Assistante Director', 'employee_avatar': 'http://i.pravatar.cc/150?img=1' },
+      { 'employee_name': '', 'employee_position': 'Administration', 'employee_avatar': 'http://i.pravatar.cc/150?img=2'}
+    ],
+    'departments': [
+      {'name': 'Marketing', 'class': 'dep-a', 'children': [
+        { 'employee_name': '', 'employee_position': 'Marketing 1', 'employee_avatar': 'http://i.pravatar.cc/150?img=3' },
+        { 'employee_name': '', 'employee_position': 'Marketing 2', 'employee_avatar': 'http://i.pravatar.cc/150?img=4' },
+        { 'employee_name': '', 'employee_position': 'Marketing 3', 'employee_avatar': 'http://i.pravatar.cc/150?img=5' },
+        { 'employee_name': '', 'employee_position': 'Marketing 4', 'employee_avatar': 'http://i.pravatar.cc/150?img=8' },
+        { 'employee_name': '', 'employee_position': 'Marketing 5', 'employee_avatar': 'http://i.pravatar.cc/150?img=9' }
+      ]},
+      {'name': 'Technical', 'class': 'dep-b', 'children': [
+        { 'employee_name': '', 'employee_position': 'Technical 1', 'employee_avatar': 'http://i.pravatar.cc/150?img=10' },
+        { 'employee_name': '', 'employee_position': 'Technical 2', 'employee_avatar': 'http://i.pravatar.cc/150?img=11' },
+        { 'employee_name': '', 'employee_position': 'Technical 3', 'employee_avatar': 'http://i.pravatar.cc/150?img=12' },
+        { 'employee_name': '', 'employee_position': 'Technical 4', 'employee_avatar': 'http://i.pravatar.cc/150?img=13' }
+      ]},
+      {'name': 'QA', 'class': 'dep-c', 'children': [
+        { 'employee_name': '', 'employee_position': 'QA 1', 'employee_avatar': 'http://i.pravatar.cc/150?img=14' },
+        { 'employee_name': '', 'employee_position': 'QA 2', 'employee_avatar': 'http://i.pravatar.cc/150?img=15' },
+        { 'employee_name': '', 'employee_position': 'QA 3', 'employee_avatar': 'http://i.pravatar.cc/150?img=16' },
+        { 'employee_name': '', 'employee_position': 'QA 4', 'employee_avatar': 'http://i.pravatar.cc/150?img=17' }
+      ]},
+      {'name': 'Sales', 'class': 'dep-d', 'children': [
+        { 'employee_name': '', 'employee_position': 'Sales 1', 'employee_avatar': 'http://i.pravatar.cc/150?img=18' },
+        { 'employee_name': '', 'employee_position': 'Sales 2', 'employee_avatar': 'http://i.pravatar.cc/150?img=19' },
+        { 'employee_name': '', 'employee_position': 'Sales 3', 'employee_avatar': 'http://i.pravatar.cc/150?img=20' },
+        { 'employee_name': '', 'employee_position': 'Sales 4', 'employee_avatar': 'http://i.pravatar.cc/150?img=21' },
+        { 'employee_name': '', 'employee_position': 'Sales 5', 'employee_avatar': 'http://i.pravatar.cc/150?img=22' },
+        { 'employee_name': '', 'employee_position': 'Sales 6', 'employee_avatar': 'http://i.pravatar.cc/150?img=23' }
+      ]},
+      {'name': 'Finance', 'class': 'dep-e', 'children': [
+        { 'employee_name': '', 'employee_position': 'Finance 1', 'employee_avatar': 'http://i.pravatar.cc/150?img=24' },
+        { 'employee_name': '', 'employee_position': 'Finance 2', 'employee_avatar': 'http://i.pravatar.cc/150?img=25' },
+        { 'employee_name': '', 'employee_position': 'Finance 3', 'employee_avatar': 'http://i.pravatar.cc/150?img=26' }
+      ]}
+    ]
+  };
+
   constructor() { }
 
   ngOnInit() {
