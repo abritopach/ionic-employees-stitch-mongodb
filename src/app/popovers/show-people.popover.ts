e9e4b642058cc0a25a023b08130252b435fec58f
Adrián Brito Pacheco <adrbrpa1988@gmail.com>
2018-10-25 13:27:28 +0200

    Updated show people popover styles

--- src/app/popovers/show-people.popover.ts
+++ src/app/popovers/show-people.popover.ts
@@ -10,13 +10,13 @@ import { PopoverController } from '@ionic/angular';
 })
 export class ShowPeoplePopoverComponent implements OnInit {
 
-  people: [
+  people: any = [
     {avatar: 'http://i.pravatar.cc/150?img=7'},
-    {avatar: 'http://i.pravatar.cc/150?img=7'},
-    {avatar: 'http://i.pravatar.cc/150?img=7'},
-    {avatar: 'http://i.pravatar.cc/150?img=7'},
-    {avatar: 'http://i.pravatar.cc/150?img=7'},
-    {avatar: 'http://i.pravatar.cc/150?img=7'}
+    {avatar: 'http://i.pravatar.cc/150?img=8'},
+    {avatar: 'http://i.pravatar.cc/150?img=9'},
+    {avatar: 'http://i.pravatar.cc/150?img=10'},
+    {avatar: 'http://i.pravatar.cc/150?img=11'},
+    {avatar: 'http://i.pravatar.cc/150?img=12'}
   ];
 
   constructor(private popoverCtrl: PopoverController) {
