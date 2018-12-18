5866f5ca71b385de3446f2d549bff4b8526a9c22
Adrián Brito Pacheco <adrbrpa1988@gmail.com>
2018-12-18 11:43:11 +0100

    Fixed #1

--- src/app/public/login/login.page.ts
+++ src/app/public/login/login.page.ts
@@ -1,13 +1,15 @@
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
-import { StitchMongoService, AuthenticationService } from './../../services';
+import { StitchMongoService, AuthenticationService, IziToastService } from './../../services';
 
 import { ObjectId } from 'bson';
 
 import { Router } from '@angular/router';
 
 import config from '../../config/config';
+
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -18,9 +20,11 @@ export class LoginPage implements OnInit {
 
   loginForm: FormGroup;
   avatar = null;
+  loading: any;
 
   constructor(private formBuilder: FormBuilder, private stitchMongoService: StitchMongoService,
-              private authService: AuthenticationService, private router: Router) {
+              private authService: AuthenticationService, private router: Router, private iziToast: IziToastService,
+              private loadingCtrl: LoadingController) {
     this.createForm();
   }
 
@@ -37,6 +41,7 @@ export class LoginPage implements OnInit {
   loginFormSubmit() {
     console.log('HomePage::loginFormSubmit() | method called');
     // this.stichMongoService.login(this.loginForm.value);
+    this.presentLoading();
     this.stitchMongoService.login(this.loginForm.value).then(authedId => {
       // console.log(authedId);
       // console.log(`successfully logged in with id: ${authedId.id}`);
@@ -44,11 +49,33 @@ export class LoginPage implements OnInit {
         this.stitchMongoService.find(config.COLLECTION_KEY, {user_id: new ObjectId(authedId.id)}).then(employee => {
           // console.log(employee);
           this.avatar = employee[0]['avatar'];
+          if (this.loading !== null) {
+            this.dismissLoading();
+          }
           setTimeout(() => this.router.navigateByUrl('/home'), 2000);
         });
       });
     })
-    .catch(err => console.error(`login failed with error: ${err}`));
+    .catch(err => {
+      if (this.loading !== null) {
+        this.dismissLoading();
+      }
+      this.iziToast.show('Login', 'You have entered an invalid username or password.', 'red', 'ico-error', 'assets/avatar.png');
+      console.error(`login failed with error: ${err}`);
+    });
+  }
+
+  async presentLoading() {
+    this.loading = await this.loadingCtrl.create({
+      message: 'Please wait, authenticating user...',
+    });
+
+    return await this.loading.present();
+  }
+
+  async dismissLoading() {
+    this.loading.dismiss();
+    this.loading = null;
   }
 
 }
