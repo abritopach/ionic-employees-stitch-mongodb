53abe7f140b6a03c94c31bfde88a9e9d7d0c14a8
abritopach <adrbrpa1988@gmail.com>
2019-02-07 17:12:35 +0100

    Fix minors problems

--- src/app/notes-list/notes-list.page.ts
+++ src/app/notes-list/notes-list.page.ts
@@ -9,6 +9,7 @@ import { Note } from '../models/note.model';
 import { StitchMongoService } from '../services';
 
 import { Router } from '@angular/router';
+import { Todo } from '../models/todo.model';
 
 
 @Component({
@@ -22,17 +23,25 @@ export class NotesListPage implements OnInit {
 
   constructor(private storage: Storage, private stitchMongoService: StitchMongoService, private router: Router) { }
 
-  ngOnInit() {
+  ionViewWillEnter() {
+    console.log('NotesListPage::ionViewWillEnter() | method called');
     this.storage.get(config.TOKEN_KEY).then(res => {
       if (res) {
         const objectId = new ObjectId(res);
         this.stitchMongoService.find(config.COLLECTION_KEY, {user_id: objectId}).then(result => {
           if ((result.length !== 0) && (typeof result[0]['notes'] !== 'undefined')) {
             this.notes = result[0]['notes'];
+            this.notes = this.notes.map(note => {
+              return { ...note, todos: note.todos.filter(todo => !todo.complete) };
+            });
           }
         });
       }
     });
+  }
+
+  ngOnInit() {
+    console.log('NotesListPage::ngOnInit() | method called');
   }
 
 }
