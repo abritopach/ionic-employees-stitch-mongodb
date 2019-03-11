c128c9868edfcab3121a73e7b5cc5508cc457e4b
abritopach <adrbrpa1988@gmail.com>
2019-03-11 13:49:09 +0100

    Updated design reminder modal

--- src/app/modals/reminder-modal/reminder-modal.component.ts
+++ src/app/modals/reminder-modal/reminder-modal.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ModalController, NavParams } from '@ionic/angular';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
+import * as moment from 'moment';
+
 @Component({
   selector: 'app-reminder-modal',
   templateUrl: './reminder-modal.component.html',
@@ -11,6 +13,14 @@ export class ReminderModalComponent implements OnInit {
 
   modalTitle: '';
   reminderForm: FormGroup;
+  showHourItems = true;
+  showLocationItems = false;
+
+  dateOptions = {
+    today: moment().toDate(),
+    tomorrow: moment(new Date()).add(1, 'days').toDate(),
+    nextMonday: moment(new Date()).add(7, 'days').toDate()
+  };
 
   constructor(private modalCtrl: ModalController, private navParams: NavParams, private formBuilder: FormBuilder) {
     this.createForm();
@@ -27,6 +37,8 @@ export class ReminderModalComponent implements OnInit {
       date: new FormControl('', Validators.required),
       hour: new FormControl('', Validators.required),
       frequency: new FormControl('', Validators.required),
+      location: new FormControl('', Validators.required),
+      customDate: new FormControl('', Validators.required),
     });
   }
 
@@ -38,7 +50,28 @@ export class ReminderModalComponent implements OnInit {
   }
 
   reminderFormSubmit() {
-    console.log('ReminderModalComponent::reminderFormSubmit | method called', this.reminderForm.value);
+    console.log('ReminderModalComponent::reminderFormSubmit() | method called', this.reminderForm.value);
   }
 
+  ionChangeHour(event) {
+    console.log('ReminderModalComponent::ionChangeHour() | method called', event);
+    this.showLocationItems = !this.showLocationItems;
+  }
+
+  ionChangeLocation(event) {
+    console.log('ReminderModalComponent::ionChangeLocation() | method called', event);
+    this.showHourItems = !this.showHourItems;
+  }
+
+  selectedDate(date) {
+    console.log('ReminderModalComponent::selectedDate() | method called', date);
+  }
+
+  selectedHour(hour) {
+    console.log('ReminderModalComponent::selectedHour() | method called', hour);
+  }
+
+  selectedFrequency(frequency) {
+    console.log('ReminderModalComponent::selectedFrequency() | method called', frequency);
+  }
 }
