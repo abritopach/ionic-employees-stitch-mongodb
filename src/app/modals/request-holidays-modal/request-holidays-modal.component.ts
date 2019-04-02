247b21fb5d2b79b2e01b82c25597469ecd0c4e84
abritopach <adrbrpa1988@gmail.com>
2019-04-02 10:55:07 +0200

    Updated holidays page style

--- src/app/modals/request-holidays-modal/request-holidays-modal.component.ts
+++ src/app/modals/request-holidays-modal/request-holidays-modal.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Validators, FormControl, FormBuilder, FormGroup } from '@angular/forms';
 import config from '../../config/config';
 import { StitchMongoService } from '../../services/stitch-mongo.service';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-request-holidays-modal',
@@ -13,7 +14,7 @@ export class RequestHolidaysModalComponent implements OnInit {
   requestHolidaysForm: FormGroup;
   employees: any;
 
-  constructor(private formBuilder: FormBuilder, private stitchMongoService: StitchMongoService) {
+  constructor(private formBuilder: FormBuilder, private stitchMongoService: StitchMongoService, private modalCtrl: ModalController) {
     this.createForm();
   }
 
@@ -38,7 +39,15 @@ export class RequestHolidaysModalComponent implements OnInit {
     this.stitchMongoService.find(config.COLLECTION_KEY, {})
     .then(docs => {
       this.employees = docs;
+      console.log('employees', this.employees);
     });
+  }
+
+  dismiss() {
+    // Using the injected ModalController this page
+    // can "dismiss" itself and pass back data.
+    // console.log('dismiss', data);
+    this.modalCtrl.dismiss(this.requestHolidaysForm.value);
   }
 
 }
